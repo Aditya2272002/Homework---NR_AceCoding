4026ce88448845187184d3671f17a137f57acb96
Aditya Maurya <adiutube22@gmail.com>
2022-02-19 15:44:07 +0530

    TR

--- DynamicProgramming/chainMultiplication.cpp
+++ DynamicProgramming/chainMultiplication.cpp
@@ -1,6 +1,25 @@
 #include<bits./stdc++.h>
 using namespace std;
 
+int dp[1000][1000];  // Time :- N*N
+int go1(int arr[],int start,int end){
+   if(start >= end){
+      return 0;
+   }
+   if(dp[start][end] != -1){
+      return dp[start][end];
+   }
+
+   int ans = INT_MAX;
+   for(int k=start;k<end;k++){
+      int firstPart = go1(arr,start,k);
+      int secondPart = go1(arr,k+1,end);
+      ans = min(ans,firstPart + secondPart + arr[start] * arr[k+1] * arr[end+1]);
+   }  
+   return dp[start][end] = ans;
+}
+
+
 //Time :- 2^N 
 int go(int arr[],int start,int end){
    if(start >= end){
@@ -22,5 +41,8 @@ int main()
    int start = 0;
    int end = n-2;
    cout<<go(arr,start,end)<<"\n";
+
+   memset(dp,-1,sizeof(dp));
+   
    return 0;
 }
